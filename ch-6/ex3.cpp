eaa73920d2fc484a9663dd3bd97145d310cfd7b8
NelsonWang <wangnelson2@gmail.com>
2022-10-16 13:23:16 -0400

    Random Progress form Nelson :tada:

--- ch-6/ex3.cpp
+++ ch-6/ex3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int fact(int num){
     // Returns the factorial of the input
     int factRes = 1;
-    for (int i=2; i<num ; i++){
+    for (int i=2; i<=num ; i++){
         factRes *=i;
     }
     return factRes;
@@ -15,5 +15,8 @@ int fact(int num){
 int main(){
     cout << "Testing Factorial with 4" <<endl;
     cout << fact(4) << endl;
+
+    cout << "Testing Factorial with 5" <<endl;
+    cout << fact(5) << endl;
     return  0;
 }
