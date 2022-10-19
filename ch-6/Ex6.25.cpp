0f1950d1988439ba3f62d35b92300563235912d0
DevlinMckenna <61163057+DevlinMckenna@users.noreply.github.com>
2022-10-19 15:31:16 -0400

    Demonstrates sending args to main via terminal

---
+++ ch-6/Ex6.25.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "agrument count is:  " << argc
+		<< " arguments:" << "\n";
+
+	cout<<"file name: "<< argv[0]<<endl;
+		cout <<"concatonating 2nd and 3rd arguments: "; 
+		 cout <<argv[1]<<argv[2]<<endl;
+	       	
+
+	return 0;
+}
+
