d2632d1d03d021aaf25c6e94cbcd6d1e15aa76fb
CesarIgnacio <cesarignac@gmail.com>
2022-10-21 14:22:39 -0400

    Update

--- ch-6/ex6.3cesar.cpp
+++ ch-6/ex6.3cesar.cpp
@@ -1,7 +1,9 @@
 #include "ex6.3cesar.hpp"
+#include<iostream>
 
 // Calculates factorial using recursive method
 int fact(int num) {
+    std::cout << "fact() called" << std::endl;
     if (num < 2)
         return num;
     return num * fact(num - 1);
