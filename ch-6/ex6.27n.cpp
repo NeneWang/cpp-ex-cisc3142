5207fb255cea633be6cf6c4d305e6ec30e936917
NelsonWang <wangnelson2@gmail.com>
2022-10-22 12:20:04 -0400

    Random Progress form Nelson :tada:

--- ch-6/ex6.27n.cpp
+++ ch-6/ex6.27n.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include<vector>
+
 using namespace std;
-int initializer_list_n(){
-    return 10;
+int initializer_list_n(vector<int> myvect){
+    
+    int sum = 0;
+    for(auto it = myvect.begin(); it != myvect.end(); ++it){
+        sum += *it;
+    }
+
+    return sum;
+
 }
--- ch-6/test_unit.cpp
+++ ch-6/test_unit.cpp
@@ -2,6 +2,9 @@
 #include "ex21.cpp"
 #include "ex6.27n.cpp"
 
+
+// Test this from the root make
+
 void test21(void){
 
     int val1= 21;
@@ -16,17 +19,29 @@ void test21(void){
     TEST_CHECK_(getLarger(val1, val2p) == (val1), "add");
 }
 
-void c6ex27(void){
+void c6ex27a(void){
 
-    int result = 10;
-    int list[4] = {1, 2, 3, 4}; //Sums into 3+7 = 10
-    TEST_CHECK_(initializer_list_n() == (result), "Should be 10");
+    int result = 20;
+    // int list[4] = {1, 2, 3, 4}; //Sums into 3+7 = 10
+    vector<int> myVect(10, 2);
+    TEST_CHECK_(initializer_list_n(myVect) == (result), "Should be 10");
+
+}
+
+void c6ex27b(void){
+
+    int result = 20;
+    // int list[4] = {1, 2, 3, 4}; //Sums into 3+7 = 10
+    vector<int> myVect(10, 2);
+    TEST_CHECK_(initializer_list_n(myVect) == (result), "Should be 5");
+    // cout << initializer_list_n(list2) << endl;
 
 }
 
 
 TEST_LIST = {
     {"Test Ex.21", test21},
-    {"Testing c6ex27", c6ex27},
+    {"Testing c6ex27a", c6ex27a},
+    {"Testing c6ex27b", c6ex27b},
     {0}
 };
