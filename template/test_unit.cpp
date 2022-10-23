61fc27601abc4639b7e63809e31c140282a91c18
NelsonWang <wangnelson2@gmail.com>
2022-10-23 19:37:55 -0400

    Random Progress form Nelson :tada:

---
+++ template/test_unit.cpp
@@ -0,0 +1,21 @@
+#include "acutest.hpp"
+#include "act-nelson.cpp"
+#include "utils.cpp"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+void test_p4(void){
+    vector<int> res = p4(2); //Should return 2
+    TEST_ASSERT_(res.at(0) == 4, "Checking value");
+    loop_vect(res);   
+}
+
+
+TEST_LIST = {
+    {"test_p4", test_p4},
+    {0}};
