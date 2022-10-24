6e90208cc5b544d8eda90ef1305519e89626848b
NelsonWang <wangnelson2@gmail.com>
2022-10-24 01:06:39 -0400

    Random Progress form Nelson :tada:

--- exam/try-catch.cpp
+++ exam/try-catch.cpp
@@ -2,6 +2,17 @@
 #include <cstdio>
 
 using namespace std;
+#include <stdexcept>
+
+class Exception : public runtime_error {
+public:
+    // Defining constructor of class Exception
+    // that passes a string message to the runtime_error class
+    Exception()
+        : runtime_error("Math error: Attempted to divide by Zero\n")
+    {
+    }
+};
 
 int main()
 {
@@ -11,10 +22,11 @@ int main()
     {
         try
         {
-            printf("Dividing %d and %d", item1, item2);
-            cout << item1/item2 << endl;
+            printf("Dividing %d and %d is: ", item1, item2);
+            int result = item1/item2;
+            cout << result << endl;
         }
-        catch (runtime_error& e)
+        catch (Exception& e)
         {
             cout << "Exception occurred" << endl << e.what();
             break;
